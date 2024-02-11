b0a644ace8ffd7ae92fba650489cd2a1c5bb6c9f
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2024-02-11 13:36:52 -0800

    Initial Commit

---
+++ xcodetest/xcodetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodetestApp.swift
+//  xcodetest
+//
+//  Created by Edwin Lee on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
